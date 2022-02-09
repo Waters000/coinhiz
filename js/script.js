0783bcc1be3c2a5ed1bf76700174357077d6a209
Jonathan McDonnell <mcdonnell.jonathan@yahoo.com>
2022-02-08 19:23:15 -0500

    Disable twitter api for testing

--- js/script.js
+++ js/script.js
@@ -6,158 +6,158 @@ var scrollContainerEl = document.querySelector(".media-scroller-container");
 var apiUrl = "https://api.twitter.com/1.1/search/tweets.json?q=%23crypto&src=typed_query";
 var apiBearerToken = "AAAAAAAAAAAAAAAAAAAAALljYwEAAAAASl%2BKPo2KBcpLkazky1jIgCG%2FzeY%3DEZJ4YlPiXm8KisMDSM8TEbXfaSZoS94eQj9h19cXqm3LjMJLPZ";
 
-// var twitterRes = {
-//   data: [
-//     {
-//       author_id: '1723531020',
-//       id: '1490430366930411523',
-//       text: 'RT @crypto_dago: The most important principle of journalism is to show both sides of the story. 99% of the news is guilty of showing only o…'
-//     },
-//     {
-//       author_id: '1267773564352069634',
-//       id: '1490430366234169344',
-//       text: '#valerstudios #VLR #blockchain #cryptocurrency #technology #bitcoin #money #crypto #Binance #BNB #cryptocurrencies #fintech @Valerstudios https://t.co/WKhOgXnLGl'
-//     },
-//     {
-//       author_id: '1465309920891265031',
-//       id: '1490430362261938177',
-//       text: 'Everyone check out #crypto services of https://t.co/XxcyJqDnrN! I am so hyped to get in the whitelist for @ChoiseCom MetaFi 🤩🤩\n' +
-//         'Invite your friends to get more! https://t.co/azcBR67KfU'
-//     },
-//     {
-//       author_id: '1482347600305078281',
-//       id: '1490430362006216704',
-//       text: 'RT @KriptoCeltics: Şubat Ayı Çekilişi 🎺\n' +
-//         '\n' +
-//         'Ödül 4.000TL\n' +
-//         '\n' +
-//         '✅Takip, \n' +
-//         '✅RT+Beğeni\n' +
-//         '\n' +
-//         '#ValentinesDay #SevgililerGünü #BTC  #Ethereum #BNB  #Crypto #B…'
-//     },
-//     {
-//       author_id: '1468881170691760135',
-//       id: '1490430361733640199',
-//       text: '🦧 @fatapeclub Roadmap 2.0 step 3 🦧\n'   +
-//         '\n' +
-//         '📣 Exclusive Live Conferences For #FatApeClub #HODLers 📣\n' +
-//         '\n' +
-//         '💎 Rewarding the #DiamondHands 💎\n' +
-//         '\n' +
-//         '👀 What live conference subjects would you like to see? #Crypto ? #NFTs ? #Business ? 👀\n' +
-//         '\n' +
-//         '#FACfollowFAC #NFT #NFTCommunity https://t.co/4fRIAnfMfC'
-//     },
-//     {
-//       author_id: '1487613831287566336',
-//       id: '1490430360110628867',
-//       text: 'Magandang balita! Nagkaroon ang web3 streaming service ng Dehub ng isang strategic partnership sa ritestream na ilalaunch ngayong February 22, 2022!\n' +
-//         '\n' +
-//         '#tothemoon #crypto #dehub #altcoin #metaverse https://t.co/b5gEnY00cn'
-//     },
-//     {
-//       author_id: '1459934042086940673',
-//       id: '1490430359263084546',
-//       text: 'RT @MyRichFarm: The reward rises to 8⃣USDT!🤩🤩\n' +
-//         '📌$1,000,000 for Sharing\n' +
-//         '📌LP was locked\n' +
-//         '📌Completed the audit \n' +
-//         '📌Earlier in and higher returns\n' +
-//         '📌…'
-//     },
-//     {
-//       author_id: '1470096558658138112',
-//       id: '1490430358889848838',
-//       text: 'Jig0lo olup ayda 2500-5000 arası para kazanmak için burdan kayıt olabilirsiniz : https://t.co/EMupq5y1XQ \n' +
-//         '\n' +
-//         '#tuerkifsaalemi #türkifşa #turkifsa #MahmutOzerİstifa #millişerefsiz #cemyilmaz #pazartesi #pars #Berna #yekta #ıspartaelektrikistiyor #SoemestrTatiliuzatilsin #Crypto #sex https://t.co/zAu7KNbZLh'
-//     },
-//     {
-//       author_id: '1479274498578919432',
-//       id: '1490430356822106116',
-//       text: 'RT @hiRavenCrypto: I just want to remind you\n' +
-//         '“Crypto don’t sleep but Bank do”🔥\n' +
-//         '\n' +
-//         '#Crypto #Altcoins'
-//     },
-//     {
-//       author_id: '1421117552399314946',
-//       id: '1490430355857281024',
-//       text: "RT @SerpentXSF: We ended the week around $40.4k so I wouldn't be surprised if we pull back before opening tomorrow. Looks like something tr…"
-//     }
-//   ],
-//   includes: {
-//     users: [
-//       {
-//         name: 'Crypto_Dago',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1389001527210221572/LnT6Y0qT_normal.jpg',
-//         username: 'crypto_dago',
-//         id: '1723531020'
-//       },
-//       {
-//         name: 'Yuli',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1267774202008031233/KZTFh1Os_normal.jpg',
-//         username: 'cryptofarma',
-//         id: '1267773564352069634'
-//       },
-//       {
-//         name: 'kinanthi',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1465310754752503809/ccYDGrh6_normal.jpg',
-//         username: 'kinanth14439230',
-//         id: '1465309920891265031'
-//       },
-//       {
-//         name: 'nft cenifer',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1482347916597633029/b_muEm1H_normal.jpg',
-//         username: 'myssteery',
-//         id: '1482347600305078281'
-//       },
-//       {
-//         name: 'ThatNFTGuy',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1489720939210280960/2WNdXaqh_normal.jpg',
-//         username: 'ThatNFTGuyy',
-//         id: '1468881170691760135'
-//       },
-//       {
-//         name: 'DeHub Philippines',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1487613881287843841/7aW3HBz__normal.png',
-//         username: 'DehubP',
-//         id: '1487613831287566336'
-//       },
-//       {
-//         name: 'Dionne Frisby',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1462659667885051905/m6ObAeKl_normal.jpg',
-//         username: 'FrisbyDionne',
-//         id: '1459934042086940673'
-//       },
-//       {
-//         name: 'Mersinli Aysu',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1470097292300632066/YFsmG4oZ_normal.jpg',
-//         username: 'aysu_mersinli',
-//         id: '1470096558658138112'
-//       },
-//       {
-//         name: 'ابن جدددة',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1479277759352479745/EzVtv7AF_normal.jpg',
-//         username: 'AJdddt',
-//         id: '1479274498578919432'
-//       },
-//       {
-//         name: 'M C',
-//         profile_image_url: 'https://pbs.twimg.com/profile_images/1454239020431462400/tuM1YpLt_normal.jpg',
-//         username: 'MC78970480',
-//         id: '1421117552399314946'
-//       }
-//     ]
-//   },
-//   meta: {
-//     newest_id: '1490430366930411523',
-//     oldest_id: '1490430355857281024',
-//     result_count: 10,
-//     next_token: 'b26v89c19zqg8o3fpe74w2kxtrw0amj40feon4f38r4ot'
-//   }
-// }
+var twitterRes = {
+  data: [
+    {
+      author_id: '1723531020',
+      id: '1490430366930411523',
+      text: 'RT @crypto_dago: The most important principle of journalism is to show both sides of the story. 99% of the news is guilty of showing only o…'
+    },
+    {
+      author_id: '1267773564352069634',
+      id: '1490430366234169344',
+      text: '#valerstudios #VLR #blockchain #cryptocurrency #technology #bitcoin #money #crypto #Binance #BNB #cryptocurrencies #fintech @Valerstudios https://t.co/WKhOgXnLGl'
+    },
+    {
+      author_id: '1465309920891265031',
+      id: '1490430362261938177',
+      text: 'Everyone check out #crypto services of https://t.co/XxcyJqDnrN! I am so hyped to get in the whitelist for @ChoiseCom MetaFi 🤩🤩\n' +
+        'Invite your friends to get more! https://t.co/azcBR67KfU'
+    },
+    {
+      author_id: '1482347600305078281',
+      id: '1490430362006216704',
+      text: 'RT @KriptoCeltics: Şubat Ayı Çekilişi 🎺\n' +
+        '\n' +
+        'Ödül 4.000TL\n' +
+        '\n' +
+        '✅Takip, \n' +
+        '✅RT+Beğeni\n' +
+        '\n' +
+        '#ValentinesDay #SevgililerGünü #BTC  #Ethereum #BNB  #Crypto #B…'
+    },
+    {
+      author_id: '1468881170691760135',
+      id: '1490430361733640199',
+      text: '🦧 @fatapeclub Roadmap 2.0 step 3 🦧\n'   +
+        '\n' +
+        '📣 Exclusive Live Conferences For #FatApeClub #HODLers 📣\n' +
+        '\n' +
+        '💎 Rewarding the #DiamondHands 💎\n' +
+        '\n' +
+        '👀 What live conference subjects would you like to see? #Crypto ? #NFTs ? #Business ? 👀\n' +
+        '\n' +
+        '#FACfollowFAC #NFT #NFTCommunity https://t.co/4fRIAnfMfC'
+    },
+    {
+      author_id: '1487613831287566336',
+      id: '1490430360110628867',
+      text: 'Magandang balita! Nagkaroon ang web3 streaming service ng Dehub ng isang strategic partnership sa ritestream na ilalaunch ngayong February 22, 2022!\n' +
+        '\n' +
+        '#tothemoon #crypto #dehub #altcoin #metaverse https://t.co/b5gEnY00cn'
+    },
+    {
+      author_id: '1459934042086940673',
+      id: '1490430359263084546',
+      text: 'RT @MyRichFarm: The reward rises to 8⃣USDT!🤩🤩\n' +
+        '📌$1,000,000 for Sharing\n' +
+        '📌LP was locked\n' +
+        '📌Completed the audit \n' +
+        '📌Earlier in and higher returns\n' +
+        '📌…'
+    },
+    {
+      author_id: '1470096558658138112',
+      id: '1490430358889848838',
+      text: 'Jig0lo olup ayda 2500-5000 arası para kazanmak için burdan kayıt olabilirsiniz : https://t.co/EMupq5y1XQ \n' +
+        '\n' +
+        '#tuerkifsaalemi #türkifşa #turkifsa #MahmutOzerİstifa #millişerefsiz #cemyilmaz #pazartesi #pars #Berna #yekta #ıspartaelektrikistiyor #SoemestrTatiliuzatilsin #Crypto #sex https://t.co/zAu7KNbZLh'
+    },
+    {
+      author_id: '1479274498578919432',
+      id: '1490430356822106116',
+      text: 'RT @hiRavenCrypto: I just want to remind you\n' +
+        '“Crypto don’t sleep but Bank do”🔥\n' +
+        '\n' +
+        '#Crypto #Altcoins'
+    },
+    {
+      author_id: '1421117552399314946',
+      id: '1490430355857281024',
+      text: "RT @SerpentXSF: We ended the week around $40.4k so I wouldn't be surprised if we pull back before opening tomorrow. Looks like something tr…"
+    }
+  ],
+  includes: {
+    users: [
+      {
+        name: 'Crypto_Dago',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1389001527210221572/LnT6Y0qT_normal.jpg',
+        username: 'crypto_dago',
+        id: '1723531020'
+      },
+      {
+        name: 'Yuli',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1267774202008031233/KZTFh1Os_normal.jpg',
+        username: 'cryptofarma',
+        id: '1267773564352069634'
+      },
+      {
+        name: 'kinanthi',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1465310754752503809/ccYDGrh6_normal.jpg',
+        username: 'kinanth14439230',
+        id: '1465309920891265031'
+      },
+      {
+        name: 'nft cenifer',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1482347916597633029/b_muEm1H_normal.jpg',
+        username: 'myssteery',
+        id: '1482347600305078281'
+      },
+      {
+        name: 'ThatNFTGuy',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1489720939210280960/2WNdXaqh_normal.jpg',
+        username: 'ThatNFTGuyy',
+        id: '1468881170691760135'
+      },
+      {
+        name: 'DeHub Philippines',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1487613881287843841/7aW3HBz__normal.png',
+        username: 'DehubP',
+        id: '1487613831287566336'
+      },
+      {
+        name: 'Dionne Frisby',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1462659667885051905/m6ObAeKl_normal.jpg',
+        username: 'FrisbyDionne',
+        id: '1459934042086940673'
+      },
+      {
+        name: 'Mersinli Aysu',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1470097292300632066/YFsmG4oZ_normal.jpg',
+        username: 'aysu_mersinli',
+        id: '1470096558658138112'
+      },
+      {
+        name: 'ابن جدددة',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1479277759352479745/EzVtv7AF_normal.jpg',
+        username: 'AJdddt',
+        id: '1479274498578919432'
+      },
+      {
+        name: 'M C',
+        profile_image_url: 'https://pbs.twimg.com/profile_images/1454239020431462400/tuM1YpLt_normal.jpg',
+        username: 'MC78970480',
+        id: '1421117552399314946'
+      }
+    ]
+  },
+  meta: {
+    newest_id: '1490430366930411523',
+    oldest_id: '1490430355857281024',
+    result_count: 10,
+    next_token: 'b26v89c19zqg8o3fpe74w2kxtrw0amj40feon4f38r4ot'
+  }
+}
 
 // set up variables for functions.
 var crypto;
@@ -352,6 +352,6 @@ scrollEl.scrollTo({
 scrollIntervalHandler(true, false);
 
 // getCrypto();
-
-altGetReq();
+populateMediaScroller(twitterRes)
+// altGetReq();
 scrollContainerEl.addEventListener("click", scrollButtonHandler);
