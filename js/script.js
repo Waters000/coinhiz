c07fa283378e03c23496d90d9ce9ed4f0fbff811
Raul <jimenezraul1981@gmail.com>
2022-02-10 01:23:17 -0500

    fixed response undifined error in the cryptoPulse function

--- js/script.js
+++ js/script.js
@@ -195,7 +195,7 @@ var getCrypto = function () {
   };
 
   $.ajax(cryptoPulse).done(function (response) {
-    for (var i = 0; i < 25; i++) {
+    for (var i = 0; i < response.legend; i++) {
       if (
         window.location.pathname === "/" ||
         window.location.pathname === "/index.html" ||
